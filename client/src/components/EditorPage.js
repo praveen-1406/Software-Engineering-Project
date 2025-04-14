eb0273370109c37d64d490608cdad0e9f8e95640
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-04-14 13:00:57 +0530

    fix env variables

--- client/src/components/EditorPage.js
+++ client/src/components/EditorPage.js
@@ -114,7 +114,7 @@ function EditorPage() {
   const runCode = async () => { 
     setIsCompiling(true);
     try {
-      const response = await axios.post("http://localhost:5000/compile", {
+      const response = await axios.post(ProcessingInstruction.env.REACT_APP_BACKEND_URL, {
         code: codeRef.current,
         language: selectedLanguage,
       });
--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
